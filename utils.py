f64fc4f7f0b196426d506b5cbe5f3b3e587e87cd
Jockwang <35221804+JockWang@users.noreply.github.com>
2020-07-23 22:02:13 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,23 +4,23 @@ import random
 
 # load data and preprocessing
 def load_data():
-    with open('./data/rate_matrix.p','r') as source:
+    with open('./data/rate_matrix.p','rb') as source:
         rating = pkl.load(source).todense()
-    with open('./data/concept_embedding.p','r') as source:
+    with open('./data/concept_embedding.p','rb') as source:
         concept = np.array(pkl.load(source))
-    with open('./data/user_feature.p','r') as source:
+    with open('./data/user_feature.p','rb') as source:
         features = pkl.load(source).todense()
     features = preprocess_features(features)
-    with open('./data/adjacency_matrix.p','r') as source:
+    with open('./data/adjacency_matrix.p','rb') as source:
         adjacency = pkl.load(source).todense()
     adjacency = preprocess_adj(adjacency)
-    with open('./data/UC.p','r') as source:
+    with open('./data/UC.p','rb') as source:
         uc = pkl.load(source).todense()
     uc = preprocess_adj(uc)
-    with open('./data/user_action.p','r') as source:
+    with open('./data/user_action.p','rb') as source:
         user_action = pkl.load(source)
     negative = radom_negative_sample(user_action,rating.shape[1])
-    with open('./data/negative.p', 'w') as source:
+    with open('./data/negative.p', 'wb') as source:
         pkl.dump(negative,source)
 
     return rating,concept,features,adjacency,negative,uc
